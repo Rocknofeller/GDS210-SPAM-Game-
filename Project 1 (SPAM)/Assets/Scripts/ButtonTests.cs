3ef8bf68bc7617d675f702cc208476f002dd000b
Andy Yudosaputro <1014710@student.sae.edu.au>
2020-06-24 16:25:43 +1000

    Added Point System, Reset system
    Player one is set to WASD, player 2 can only use UpArrow, will mash head to think about how to get buttons to add/remove

--- Project 1 (SPAM)/Assets/Scripts/ButtonTests.cs
+++ Project 1 (SPAM)/Assets/Scripts/ButtonTests.cs
@@ -6,24 +6,63 @@ using UnityEngine.UI;
 
 public class ButtonTests : MonoBehaviour
 {
-    public float oneProgress = 0;
-    public float twoProgress = 0;
+    private bool spammable = false;
+    private float oneProgress = 0;
+    private float twoProgress = 0;
     public GameObject progOne;
     public GameObject progTwo;
-    
+    public GameObject winnerText;
+    private string defText;
+    public GameObject p1Text;
+    public GameObject p2Text;
+    private int p1Point = 0;
+    private int p2Point = 0;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        progOne.GetComponent<Image>().fillAmount = 0;
-        progTwo.GetComponent<Image>().fillAmount = 0;
+        defText = winnerText.GetComponent<Text>().text;
+
+        RoundStart();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (spammable == true) ButtonInputs();
         
-        if (Input.GetKeyDown(KeyCode.W))
+        //These two lines updates the bar
+        progOne.GetComponent<Image>().fillAmount = oneProgress;
+        progTwo.GetComponent<Image>().fillAmount = twoProgress;
+
+        Winner();
+    }
+
+    void RoundStart()
+    {
+        //Resets it the progress bars to 0
+        //set it seperate to start so it can be reused to reset
+        //resets the mid text as well, will replace to SPAM!
+
+        spammable = true;
+
+        progOne.GetComponent<Image>().fillAmount = 0;
+        progTwo.GetComponent<Image>().fillAmount = 0;
+        oneProgress = 0;
+        twoProgress = 0;
+        winnerText.GetComponent<Text>().text = defText;
+    }
+
+    void ButtonInputs()
+    {
+        //Player 1 can use all of WASD
+        //Probably need to find a more efficient way to write these down
+        //inputs are determined in Edit>ProjectSettings>InputManager
+        //this way the inputs aren't locked to WASD and/or arrow keys
+        //Definitely need to find a more efficient way to write this down and change it on the fly
+
+        if (Input.GetButtonDown("p1Up") | Input.GetButtonDown("p1Down") | Input.GetButtonDown("p1Left") | Input.GetButtonDown("p1Right"))
         {
             oneProgress += 0.01f;
         }
@@ -32,10 +71,45 @@ public class ButtonTests : MonoBehaviour
             twoProgress += 0.01f;
         }
 
+    }
 
-        progOne.GetComponent<Image>().fillAmount = oneProgress;
+    void Winner()
+    {
+        //victory conditions
+        //Also stops the loser's progress bar
+        //stops spamming entirely
+        if (oneProgress >= 1 && spammable == true)
+        {
+            p1Point += 1;
+            // print("Player 1 Wins");
+            winnerText.GetComponent<Text>().text = "Player 1 Wins\n Press Space to Reset";
+            StringChange();
+            spammable = false;
+        }
+        else if (twoProgress >= 1 && spammable == true)
+        {
+            p2Point += 1;
+            // print("Player 2 Wins");
+            winnerText.GetComponent<Text>().text = "Player 2 Wins\n Press Space to Reset";
+            StringChange();
+            spammable = false;
+        }
 
-        progTwo.GetComponent<Image>().fillAmount = twoProgress;
+        //little code to reset the script and the round
 
+        if (Input.GetKeyDown(KeyCode.Space) && spammable == false)
+        {
+            print("round reset");
+            RoundStart();
+        }
+
+    }
+
+    void StringChange()
+    {
+        string p1String = p1Point.ToString();
+        string p2String = p1Point.ToString();
+        p1Text.GetComponent<Text>().text = p1String;
+        p1Text.GetComponent<Text>().text = p2String;
     }
 }
