045b5819fd2d34dfd2a94f18a3446da385a802e7
Andy Yudosaputro <1014710@student.sae.edu.au>
2020-06-24 15:12:30 +1000

    Buttons! Press W for blue, Up for Red
    Tell me if there are issues

---
+++ Project 1 (SPAM)/Assets/Scripts/ButtonTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ButtonTests : MonoBehaviour
+{
+    public float oneProgress = 0;
+    public float twoProgress = 0;
+    public GameObject progOne;
+    public GameObject progTwo;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        progOne.GetComponent<Image>().fillAmount = 0;
+        progTwo.GetComponent<Image>().fillAmount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            oneProgress += 0.01f;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            twoProgress += 0.01f;
+        }
+
+
+        progOne.GetComponent<Image>().fillAmount = oneProgress;
+
+        progTwo.GetComponent<Image>().fillAmount = twoProgress;
+
+    }
+}
