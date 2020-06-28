4d9cbeb2a0dbb8ef35b5062bcc2dd2e8f8c85e91
Noel A <1015895@student.sae.edu.au>
2020-06-29 02:35:48 +1000

    Update 1.3
    Added timer to the game and the extra cheese

---
+++ Project 1 (SPAM)/Assets/Scripts/CountDownStart.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDownStart : MonoBehaviour
+{
+    public int countdownTime;
+    public Text countdownDisplay;
+    public Sprite SpamSprite;
+
+    private void Start()
+    {
+
+        StartCoroutine(CountdownToStart());
+        
+    }
+
+    IEnumerator CountdownToStart()
+    {
+        while (countdownTime >= 0) 
+        {
+            ButtonTests.spammable = false;
+
+            countdownDisplay.text = countdownTime.ToString();
+
+            yield return new WaitForSeconds(1f);
+
+            countdownTime--;  
+
+        }
+
+        SpriteRenderer spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = SpamSprite;
+
+        ButtonTests.spammable = true;
+
+        yield return new WaitForSeconds(1f);
+
+        countdownDisplay.gameObject.SetActive(false);
+
+        spriteRenderer.gameObject.SetActive(false);
+
+    }
+}
